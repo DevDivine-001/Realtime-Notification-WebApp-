3a13dd2c33c4b670ac9ebd265571693513aee6e0
Oscdivin <divineuwandu84@Gmail.com>
2024-11-21 00:04:33 +0100

    added sum icons in the card

--- src/page/card/Card.jsx
+++ src/page/card/Card.jsx
@@ -1,3 +1,5 @@
+import { FaHeart, FaShareAlt } from "react-icons/fa"
+import { SiMessenger } from "react-icons/si"
 
 const Card = () => {
     return (
@@ -6,9 +8,15 @@ const Card = () => {
                 <div className="w-full max-w-3xl p-4 flex flex-col gap-3 shadow-xl bg-white rounded-lg max-sm:w-[85%]">
                     <img src="" alt="" className="w-[400px] h-[420px] bg-[green] rounded-md" />
                     <div className="flex space-x-3">
-                        <div className="h-[40px] w-[40px] rounded-full bg-black"></div>
-                        <div className="h-[40px] w-[40px] rounded-full bg-black"></div>
-                        <div className="h-[40px] w-[40px] rounded-full bg-black"></div>
+                        <div className="h-[40px] w-[40px] rounded-full bg-white object-cover">
+                            <SiMessenger size={28} />
+                        </div>
+                        <div className="h-[40px] w-[40px] rounded-full bg-white object-cover">
+                            <FaShareAlt size={28} />
+                        </div>
+                        <div className="h-[40px] w-[40px] rounded-full bg-white object-cover">
+                            <FaHeart size={28} />
+                        </div>
 
                     </div>
 
