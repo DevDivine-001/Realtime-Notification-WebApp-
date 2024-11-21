67805668daa7b45deb5064704f91b232dfb337d4
Oscdivin <divineuwandu84@Gmail.com>
2024-11-21 01:00:56 +0100

    added sum

--- src/page/card/Card.jsx
+++ src/page/card/Card.jsx
@@ -1,29 +1,38 @@
-import { FaHeart, FaShareAlt } from "react-icons/fa"
-import { SiMessenger } from "react-icons/si"
+import { FaHeart, FaShareAlt } from "react-icons/fa";
+import { SiMessenger } from "react-icons/si";
 
 const Card = () => {
     return (
-        <div className="flex justify-center items-center ">
-            <div className="flex justify-center items-center min-h-screen ">
-                <div className="w-full max-w-3xl p-4 flex flex-col gap-3 shadow-xl bg-white rounded-lg max-sm:w-[85%]">
-                    <img src="" alt="" className="w-[400px] h-[420px] bg-[green] rounded-md" />
-                    <div className="flex space-x-3">
-                        <div className="h-[40px] w-[40px] rounded-full bg-white object-cover">
-                            <SiMessenger size={28} />
-                        </div>
-                        <div className="h-[40px] w-[40px] rounded-full bg-white object-cover">
-                            <FaShareAlt size={28} />
-                        </div>
-                        <div className="h-[40px] w-[40px] rounded-full bg-white object-cover">
-                            <FaHeart size={28} />
-                        </div>
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="flex justify-center items-center w-full">
+                <div className="w-full max-w-3xl p-4 flex flex-col gap-3 shadow-xl bg-white rounded-lg sm:max-w-lg xs:max-w-xs">
+                    {/* Image */}
+                    <img
+                        src=""
+                        alt="Card Image"
+                        className="w-full h-[330px] bg-green-500 rounded-md object-cover sm:h-[350px] xs:h-[200px]"
+                    />
+                    <div className="flex">
+                        <span>Divine</span>
 
                     </div>
 
+                    {/* Icons */}
+                    <div className="flex justify-around">
+                        <div className="flex justify-center items-center h-[40px] w-[40px] rounded-full bg-white shadow-md">
+                            <SiMessenger size={20} className="text-gray-600" />
+                        </div>
+                        <div className="flex justify-center items-center h-[40px] w-[40px] rounded-full bg-white shadow-md">
+                            <FaShareAlt size={20} className="text-gray-600" />
+                        </div>
+                        <div className="flex justify-center items-center h-[40px] w-[40px] rounded-full bg-white shadow-md">
+                            <FaHeart size={20} className="text-gray-600" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Card
+export default Card;
